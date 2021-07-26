89dcea4d3f27a768969dc2de2da478798d741b6e
SakibHasanCse <sakibhasan9641@gmail.com>
2021-07-27 00:08:36 +0600

    frontend setup

---
+++ webRTC/client/src/components/Options.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Options = ({ children }) => {
+    return (
+        <div>
+            Hello
+            { children}
+        </div>
+
+    )
+}
+
+export default Options
