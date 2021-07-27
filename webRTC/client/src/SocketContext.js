9ef04519dfeb800ff652c4dd38ee888a7feabfe0
SakibHasanCse <sakibhasan9641@gmail.com>
2021-07-27 23:38:44 +0600

    add

--- webRTC/client/src/SocketContext.js
+++ webRTC/client/src/SocketContext.js
@@ -8,28 +8,47 @@ const SocketContext = createContext();
 const socket = io('http://localhost:5000');
 
 const ContextProvider = ({ children }) => {
-    const [strem, setStrem] = useState(null);
+    const [stream, setStream] = useState(null);
     const [me, setMe] = useState('');
+    const [call, setCall] = useState({});
+    const [callAccepted, setCallAccepted] = useState(false);
+    const [callEnded, setCallEnded] = useState(false);
+
     const myVideo = useRef();
+    const userVideo = useRef();
 
     useEffect(() => {
         navigator.mediaDevices.getUserMedia({ video: true, audio: true })
             .then((currentStream) => {
-                setStrem(currentStream);
+                setStream(currentStream);
                 myVideo.current.srcObject = currentStream;
             })
             .catch(() => {
                 console.log('Failed to get current stream')
             })
 
-        socket.on('me', (id) => setMe(id))
-    })
+        socket.on('me', (id) => setMe(id));
+
+        socket.on('callUser', ({ from, name: callerName, signal }) => {
+            setCall({ isReceivedCall = true, from, name: callerName, signal })
+        })
+    }, [])
 
     const answerCall = () => {
+        setCallAccepted(true);
 
+        const peer = new Peer({ initiator: false, trickle: false, stream });
+
+        peer.on('signal', function (data) {
+            socket.emit('answercall', { signal: data, to: call.from })
+        })
+        peer.on('stream', function (currentStream) {
+            userVideo.current.srcObject = currentStream;
+        })
     }
-    const callUser = () => {
 
+    const callUser = () => {
+        setCallEnded(true);
     }
 
     const leaveCall = () => {
